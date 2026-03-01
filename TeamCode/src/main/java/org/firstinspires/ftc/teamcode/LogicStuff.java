0cb5122a9dbf4f03ac25ed424001ff0786b55ded
MisterMoe7 <calebeddy9@gmail.com>
2026-03-01 12:07:13 -0500

    Logic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LogicStuff.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class LogicStuff extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        if (!gamepad1.a) {
+            double motorSpeed = -gamepad1.left_stick_y / 2.0;
+            telemetry.addData("Motor Speed", motorSpeed);
+        } else {
+            double motorSpeed = -gamepad1.left_stick_y;
+            telemetry.addData("Motor Speed", motorSpeed);
+        }
+    }
+}
